bc4065238a5b277836ad1bad2b5939a81238ff9a
ljh415 <ljh415@gmail.com>
2020-08-31 23:51:00 +0900

    mod app.py, sign-up.py

--- api/sign-up.py
+++ api/sign-up.py
@@ -1,14 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request               # 1
 
 app          = Flask(__name__)
-app.users    = {}
-app.id_count = 1
+app.users    = {}                                       # 2
+app.id_count = 1                                        # 3
 
-@app.route("/sign-up", methods=['POST'])
+@app.route("/sign-up", methods=['POST'])                # 4
 def sign_up():
-    new_user                    = request.json
-    new_user["id"]              = app.id_count
-    app.users[app.id_count]     = new_user
-    app.id_count                = app.id_count + 1
+    new_user                    = request.json          # 5
+    new_user["id"]              = app.id_count          # 6
+    app.users[app.id_count]     = new_user              # 7
+    app.id_count                = app.id_count + 1      # 8
 
-    return jsonify(new_user)
+    return jsonify(new_user)                            # 9
